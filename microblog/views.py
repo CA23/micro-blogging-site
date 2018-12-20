7a518f0e6d72cd52869d784268fe7edd26a21b03
Chaitanya Atre <chaitanyatre@gmail.com>
2018-12-20 22:37:24 +0100

    added comment model and corresponding views and templates

--- microblog/views.py
+++ microblog/views.py
@@ -1,17 +1,24 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
-from .forms import PostForm
+from .forms import PostForm, CommentForm
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'microblog/post_list.html', {'posts': posts})
 
+
+#@login_required
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'microblog/post_detail.html', {'post': post})
 
+
+@login_required
 def post_new(request):
     form = PostForm()
     if request.method == "POST":
@@ -26,6 +33,7 @@ def post_new(request):
         form = PostForm()
     return render(request, 'microblog/post_edit.html', {'form': form})
 
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -39,3 +47,21 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'microblog/post_edit.html', {'form': form})
+
+@login_required
+def add_a_comment(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+
+        if form.is_valid():
+            comment = form.save(commit=False)
+            cmment.post = post
+            comment.save()
+
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+
+    return render(request, 'microblog/add_a_comment.html', {'form': form})
+
